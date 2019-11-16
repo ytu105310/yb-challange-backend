5388da44d13688b8dbc31b9f7980fd730f3786a2
Türkes Yunus, GHR-OSA-NEX-33 <yunus.tuerkes@swisscom.com>
2019-11-16 02:40:05 +0100

    add Websocket with API-connection

--- server.js
+++ server.js
@@ -1,35 +1,50 @@
 const WebSocket = require('ws');
 const fs = require('fs');
-const ApiController = require('./apiController');
-
+const http = require('http');
+const parseString = require('xml2js').parseString;
 
 
 class Main {
 
-	constructor() {
-    this.socket();
-  }
-  
-	socket() {
-    const apiController = new ApiController();
-    apiController.test();
+    constructor() {
+        this.socket();
+    }
 
-		const wss = new WebSocket.Server({
-			port: 8080
-		});
+    socket() {
+        var json = this.test();
+        const wss = new WebSocket.Server({
+            port: 8080
+        });
 
-		wss.on('connection', (ws) => {
+        wss.on('connection', (ws) => {
 
-			let rawdata = fs.readFileSync('livedata/nextGoal.json');
-			let student = JSON.parse(rawdata);
-			// WS Sample Method
-			ws.on('message', (message) => {
-				console.log(`Received message => ${message}`);
+            let rawdata = fs.readFileSync('livedata/nextGoal.json');
+            let student = JSON.parse(rawdata);
+            // WS Sample Method
+            ws.on('message', (message) => {
+                console.log(`Received message => ${message}`);
 
-			});
-			ws.send('ho!');
-		})
-	}
+            });
+            ws.send(json);
+        })
+    }
+
+    test() {
+        http.get('http://api.sfl.ch/xml/teamdetail/hash/df323763af6c70ccc39545bc6f50fdc5/teamxid/52591/',
+            (resp) => {
+                let data = '';
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+                // The whole response has been received. Print out the result.
+                resp.on('end', () => {
+                    parseString(data, function (err, result) {
+                        console.log(JSON.stringify(result));
+                        return JSON.stringify(result);
+                    });
+                });
+            })
+    }
 }
 
 const server = new Main();
--- testclient/client.js
+++ testclient/client.js
@@ -2,13 +2,13 @@ const url = 'ws://localhost:8080'
 const connection = new WebSocket(url)
 
 connection.onopen = () => {
-  connection.send('hey') 
+    connection.send('hey')
 }
 
 connection.onerror = (error) => {
-  console.log(`WebSocket error: ${error}`)
+    console.log(`WebSocket error: ${error}`)
 }
 
 connection.onmessage = (e) => {
-  console.log(e.data)
+    console.log(e.data)
 }
